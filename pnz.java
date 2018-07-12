5767ba63c2aebe4b8ab96ed748ae3506f96d4aed
JayaJAva <41136638+JayaJAva@users.noreply.github.com>
2018-07-12 10:18:27 +0530

    Add files via upload

--- pnz.java
+++ pnz.java
@@ -2,10 +2,16 @@ package jaya;
 import java.util.Scanner;
 public class pnz {
 	public static void main(String[] args){
+		int N=0;
 		Scanner jaya=new Scanner(System.in);
-		int n=jaya.nextInt();
-		if(n!=0){
-			if(n>0) System.out.println("Positive");
+		try{
+		 N=jaya.nextInt();
+		}catch(Exception e){
+			System.out.println("Invalid Input");
+			return;
+		}
+		if(N!=0){
+			if(N>0) System.out.println("Positive");
 			else System.out.println("Negative");
 		}
 		else System.out.println("Zero");
