b91469a114ec0159afabbe6a201a0a6b934b427a
JayaJAva <41136638+JayaJAva@users.noreply.github.com>
2018-07-12 10:23:16 +0530

    Add files via upload

---
+++ fresh.java
@@ -0,0 +1,21 @@
+package jaya;
+
+import java.util.Scanner;
+
+public class fresh {
+	public static void main(String[] args){
+		int N=0;
+		Scanner jaya=new Scanner(System.in);
+		try{
+		 N=jaya.nextInt();
+		}catch(Exception e){
+			System.out.println("Invalid Input");
+			return;
+		}
+		if(N!=0){
+			if(N>0) System.out.println("Positive");
+			else System.out.println("Negative");
+		}
+		else System.out.println("Zero");
+	}
+}
